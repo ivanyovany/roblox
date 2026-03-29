bb4ff0a470203716b308510449476947d9e2d6d7
ivanyovany <yovany_bm@hotmail.com>
2026-03-29 16:27:29 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- autofarm.lua
+++ autofarm.lua
@@ -1,6 +1,6 @@
 --[[
 BRAINROT FARMER V236 - ATTRIBUTE TARGETING + HARD PICKUP CONFIRM
-Arquitectura: FSM Estricta, DEAD terminal, recovery estable y BURST sellado seguro0s
+Arquitectura: FSM Estricta, DEAD terminal, recovery estable y BURST sellado seguro0sd
 
 CAMBIOS V236:
 1. Nuevo modo auto por atributos si no se selecciona mutacion manual.
